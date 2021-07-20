8d94abc5912f84b903959521bb28bdfebcb30aca
rosekamallove <rosekamallove@gmail.com>
2021-07-21 02:06:19 +0530

    darkTheme Toggle based on systemTheme

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -1,13 +1,25 @@
+const systemTheme = document.getElementById('system-theme');
+
+if(localStorage.getItem('system-theme') == 'true'){
+  systemTheme.checked = true;
+} else {
+  systemTheme.checked = false;
+}
+
+systemTheme.addEventListener('change', () => {
+  localStorage.setItem('system-theme' , systemTheme.checked);
+});
+
 // Immediately invoked function to set the theme on initial load
 (function () {
-  if(document.getElementById('system-theme').checked){
+  if(localStorage.getItem('system-theme') == 'true'){
     systemThemeToggle();
     return;
-  }
+  } 
   setThemeOnLoad();
 })();
 
-/* Change theme based on System-Theme */
+/* Function Declarations */
 function systemThemeToggle() {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     setTheme("theme-dark");
@@ -38,4 +50,3 @@ function setThemeOnLoad(){
     document.getElementById('slider').checked = true;
   }
 }
-
