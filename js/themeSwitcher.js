0c3127a671713c869c888cc5b7b01621c42729d9
rosekamallove <rosekamallove@gmail.com>
2021-07-21 01:27:10 +0530

    systeTheme Toggle

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -1,26 +1,41 @@
-//const slider = document.getElementById('slider');
-// function to set a given theme/color-scheme
-function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
-
-// function to toggle between light and dark theme
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
-    }
-}
-
 // Immediately invoked function to set the theme on initial load
 (function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-        document.getElementById('slider').checked = false;
-    } else {
-        setTheme('theme-light');
-        document.getElementById('slider').checked = true;
-    }
+  if(document.getElementById('system-theme').checked){
+    systemThemeToggle();
+    return;
+  }
+  setThemeOnLoad();
 })();
+
+/* Change theme based on System-Theme */
+function systemThemeToggle() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    setTheme("theme-dark");
+  } else {
+    setTheme("theme-light");
+  }
+}
+
+function setTheme(themeName) {
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-light");
+  } else {
+    setTheme("theme-dark");
+  }
+}
+
+function setThemeOnLoad(){
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark');
+    document.getElementById('slider').checked = false;
+  } else {
+    setTheme('theme-light');
+    document.getElementById('slider').checked = true;
+  }
+}
+
