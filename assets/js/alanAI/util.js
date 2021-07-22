dda73e0f85429c0ab0e4b804c0a6f26893884417
rosekamallove <rosekamallove@gmail.com>
2021-07-23 03:19:18 +0530

    Theme Toggle

--- assets/js/alanAI/util.js
+++ assets/js/alanAI/util.js
@@ -1,3 +1,7 @@
+/***********************************
+ *  @Utility Functions For Alan AI *
+ ***********************************/
+
 const downloadResume = () => {
   let resumeLink;
   if (document.getElementById("resume-link"))
@@ -9,10 +13,36 @@ const downloadResume = () => {
   }
 };
 
+const alanSystemThemeToggle = () => {
+  const systemTheme = document.getElementById("system-theme");
+  if (systemTheme.checked) {
+    systemTheme.checked = false;
+  } else {
+    systemTheme.checked = true;
+  }
+  localStorage.setItem("system-theme", systemTheme.checked);
+  console.log(systemTheme.checked);
+};
+
 const goToProjectPage = () => {
   const projectData = document.getElementById("p1");
   projectData.click();
   readProjectPage();
+};
+
+const htmlToJson = (div, obj) => {
+  if (!obj) obj = [];
+  var tag = {};
+  tag["tagName"] = div.tagName;
+  tag["children"] = [];
+  for (var i = 0; i < div.children.length; i++) {
+    tag["children"].push(htmlToJson(div.children[i]));
+  }
+  for (var i = 0; i < div.attributes.length; i++) {
+    var attr = div.attributes[i];
+    tag["@" + attr.name] = attr.value;
+  }
+  return tag;
 };
 
 const getProjectJSON = () => {};
