decb877d6026349526c6bff28c0447b6d75e9b89
rosekamallove <rosekamallove@gmail.com>
2021-07-23 02:43:16 +0530

    openSocial Media Command

--- assets/js/alanAI/init.js
+++ assets/js/alanAI/init.js
@@ -1,12 +1,32 @@
 const alanBtnInstance = alanBtn({
   key: "5c0e10ba1522cb83798eb8e6af73e2ef2e956eca572e1d8b807a3e2338fdd0dc/stage",
-  onCommand: function ({ command }) {
-    if (command === "resumeCommand") {
+  onCommand: function (commandData) {
+    if (commandData.command === "resumeCommand") {
       if (downloadResume() == "No Resume") {
         alert("Issue with Resume Link");
       } else {
         downloadResume();
       }
+    }
+    if (commandData.command === "socialMediaCommand") {
+      console.clear();
+      console.log(commandData.social);
+      openSocial(commandData.social);
+    }
+    if (commandData.command === "openProjectCommand") {
+      goToProjectPage();
+      project = getProjectJSON();
+      alanBtnInstance.callProjectApi(
+        "sendProject",
+        { data: project },
+        function (error, result) {
+          if (error) {
+            console.error(error);
+            return;
+          }
+          console.log(result);
+        }
+      );
     }
   },
   rootEl: document.getElementById("alan-btn"),
--- assets/js/alanAI/util.js
+++ assets/js/alanAI/util.js
@@ -8,3 +8,29 @@ const downloadResume = () => {
     return "No Resume";
   }
 };
+
+const goToProjectPage = () => {
+  const projectData = document.getElementById("p1");
+  projectData.click();
+  readProjectPage();
+};
+
+const getProjectJSON = () => {};
+
+const openSocial = (social) => {
+  const linkedIn = "https://linkedin.com/";
+  const gitHub = "https://github.com/";
+  if (social == "LinkedIn") {
+    const LINK = document.createElement("a");
+    LINK.href = linkedIn;
+    LINK.target = "_blank";
+    LINK.click();
+    document.body.removeChild(LINK);
+  } else if (social == "GitHub") {
+    const LINK = document.createElement("a");
+    LINK.href = gitHub;
+    LINK.target = "_blank";
+    LINK.click();
+    document.body.removeChild(LINK);
+  }
+};
